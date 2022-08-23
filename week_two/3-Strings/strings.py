bc105f6d92a2605598c23c169621d038ab41e8a5
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-08-23 19:06:13 +0300

    New update

--- week_two/3-Strings/strings.py
+++ week_two/3-Strings/strings.py
@@ -1,4 +1,16 @@
 word = "Python"
 
-print(word[4:6])
-print(word[-1:6])
+print(word[1:3]) # yt
+print(word[1:-2]) # n
+
+# my_list = [_ for _ in 'abcdefghi']
+# print(my_list)
+
+alphabet = []
+# ord represent the character unicode code, A to 65.
+# chr convert 65 to A.
+# variable alphabet get all value.
+for i in range(ord('A'), ord('Z') + 1):
+    alphabet.append(chr(i))
+print(alphabet)
+#ganesh.
